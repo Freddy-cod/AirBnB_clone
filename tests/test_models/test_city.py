81f6f9654a77a22ee10e62b4ea5419d093c6488e
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-31 13:24:12 +0200

    Create test_city.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""City classes"""
+
+import unittest
+from datetime import datetime
+import time
+from models.city import City
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+
+    """City tests"""
+
+    def setUp(self):
+        """Setting up"""
+        pass
+
+    def tearDown(self):
+        """Tearing down"""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Ressetting storages"""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instances"""
+
+        b = City()
+        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
+        self.assertIsInstance(b, City)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes"""
+        attributes = storage.attributes()["City"]
+        o = City()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
